e5924d24cbe140b0ae2cb9cfead76549a3a5d879
film00 <69621477+film00@users.noreply.github.com>
2025-02-09 16:08:36 +0700

    Update edit_car.php

--- edit_car.php
+++ edit_car.php
@@ -4,10 +4,10 @@ ob_start();
 require_once "./nav/navbar_addmin.php";
 require_once "./footer/footer.php";
 
-$servername = "localhost";
-$username = "yanreeho_yanree_db";
-$password = "B@4N+209rhMfoT";
-$dbname = "yanreeho_yanree_db";
+$servername = "**********";
+$username = "**********";
+$password = "**********";
+$dbname = "**********";
 
 
 try {
